34551ca6eb4896e0361a3b044e4558e58c112c43
Amit gupta <billamitgupta@gmail.com>
2024-02-03 15:28:11 +0530

    new build

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
